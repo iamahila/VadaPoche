c7391f6ab498399a731351fa9ecc3419fd4fbfab
iamahila <ahilachockalingam@gmail.com>
2022-04-09 10:45:08 +0530

    Initial Commit

---
+++ src/main/java/com/luv2eat/VadaPoche/VadaPocheApplication.java
@@ -0,0 +1,13 @@
+package com.luv2eat.VadaPoche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VadaPocheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VadaPocheApplication.class, args);
+	}
+
+}
